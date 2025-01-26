ba4f7d92477eb7a84bf3e067876bbadb17afbc91
David Zoubek <xzoube02@vutbr.cz>
2025-01-26 21:54:00 +0100

    Person.cs Fixed

--- EprinAppClient/ClientForm.cs
+++ EprinAppClient/ClientForm.cs
@@ -1,6 +1,7 @@
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
+using EprinAppLibrary;
 
 namespace EprinAppClient
 {
--- EprinAppLibrary/Person.cs
+++ EprinAppLibrary/Person.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EprinAppClient
+﻿namespace EprinAppLibrary
 {
     public class Person
     {
-        public int Id { get; set; }
+        public int Id {  get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
 
